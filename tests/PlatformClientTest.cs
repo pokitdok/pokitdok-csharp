d10b2314cb86e75a7e8cdb0d2b1013d96a3e4831
Daniel Baldwin <baldwindc@gmail.com>
2016-12-20 18:00:14 +0000

    PLATFORM-5297: Add tests for Serialization / DeSerialization.

--- tests/PlatformClientTest.cs
+++ tests/PlatformClientTest.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using pokitdokcsharp;
 using NUnit.Framework;
 using System.Net;
+using pokitdokcsharp;
 using pokitdokcsharp.tests.data;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 /// <summary>
 /// Platform client test.
@@ -66,5 +69,122 @@ public class PlatformClientTest
     {
         var request = oac.CreateRequest(ExampleRequests.EligibilityRequest, "http://localhost", "DELETE");
         AssertRequestDefaults(request, "DELETE");
+    }
+
+    /// <summary>
+    /// Verify base constructor
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_DefaultConstructor()
+    {
+        PokitDokException exception = new PokitDokException();
+        Assert.AreEqual("Exception of type 'pokitdokcsharp.PokitDokException' was thrown.", exception.Message); 
+    }
+
+    /// <summary>
+    /// Verify "String" Constructor
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_StringConstructor()
+    {
+        PokitDokException exception = new PokitDokException("Test Message.");
+        Assert.AreEqual("Test Message.", exception.Message); 
+    }
+
+
+    /// <summary>
+    /// Verify "String-Exception" Constructor
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_StringExceptionConstructor()
+    {
+        WebException innerException = new WebException(); 
+
+        PokitDokException exception = new PokitDokException("Test Message.", innerException);
+        Assert.AreEqual("Test Message.", exception.Message); 
+    }
+
+
+    /// <summary>
+    /// Verify "String-Exception" Constructor
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_VerifyInnerException()
+    {
+        WebException innerException = new WebException("Alternative Test Message"); 
+
+        PokitDokException exception = new PokitDokException("Test Message.", innerException);
+        Assert.AreEqual("Test Message.", exception.Message);
+        Assert.AreEqual(exception.InnerException, innerException); 
+    }
+
+    /// <summary>
+    /// Verify Default Constructor can be deserialized
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_DefaultConstructorDeSerializable()
+    {
+        // Arrange
+        var originalException = new PokitDokException(); 
+        var buffer = new byte[4096];
+        var ms = new MemoryStream(buffer);
+        var ms2 = new MemoryStream(buffer);
+        var formatter = new BinaryFormatter();
+
+        // Act
+        formatter.Serialize(ms, originalException);
+        var deserializedException = (PokitDokException)formatter.Deserialize(ms2);
+
+        // Assert
+        Assert.AreEqual(originalException.ResourceReferenceProperty, deserializedException.ResourceReferenceProperty);
+        Assert.AreEqual(originalException.Message, deserializedException.Message);
+    }
+
+
+    /// <summary>
+    /// Verify String Constructor can be Deserialized
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_StringConstructorDeSerializable()
+    {
+        // Arrange
+        var originalException = new PokitDokException("Test Message."); 
+        var buffer = new byte[4096];
+        var ms = new MemoryStream(buffer);
+        var ms2 = new MemoryStream(buffer);
+        var formatter = new BinaryFormatter();
+
+        // Act
+        formatter.Serialize(ms, originalException);
+        var deserializedException = (PokitDokException)formatter.Deserialize(ms2);
+
+        // Assert
+        Assert.AreEqual(originalException.ResourceReferenceProperty, deserializedException.ResourceReferenceProperty);
+        Assert.AreEqual(originalException.Message, deserializedException.Message);
+    }
+
+
+    /// <summary>
+    /// Verify String, Exception Constructor can be Deserialized
+    /// </summary>
+    [Test]
+    public void TestPokitDokException_StringExceptionConstructorDeSerializable()
+    {
+        // Arrange
+        var innerEx = new Exception("foo");
+        var originalException = new PokitDokException("Test Message.", innerEx); 
+        var buffer = new byte[4096];
+        var ms = new MemoryStream(buffer);
+        var ms2 = new MemoryStream(buffer);
+        var formatter = new BinaryFormatter();
+
+        // Act
+        formatter.Serialize(ms, originalException);
+        var deserializedException = (PokitDokException)formatter.Deserialize(ms2);
+
+        // Assert
+        Assert.AreEqual(originalException.ResourceReferenceProperty, deserializedException.ResourceReferenceProperty);
+        Assert.AreEqual(originalException.InnerException.Message, deserializedException.InnerException.Message);
+        Assert.AreEqual(originalException.Message, deserializedException.Message);
     }
 }
